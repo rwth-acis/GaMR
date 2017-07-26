a2f554f71caf1a2847605fe933a0f2953ca6337a
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2017-07-26 20:50:17 +0200

    improved design of the progress bar

--- Frontend/GaMR/Assets/Scripts/ProgressBar.cs
+++ Frontend/GaMR/Assets/Scripts/ProgressBar.cs
@@ -9,7 +9,15 @@ public class ProgressBar : MonoBehaviour {
 
     private Vector3 velocity = Vector3.zero;
     public float smoothTime = 0.3f;
+    [SerializeField]
+    private GameObject progressBar;
+    [SerializeField]
+    private GameObject hull;
+    [SerializeField]
+    private GameObject upperCap;
+    private float badgeHeight = 0.25f;
 
+    [SerializeField]
     private float progress = 0f;
 
     public float Progress
@@ -20,18 +28,35 @@ public class ProgressBar : MonoBehaviour {
 
     private void Start()
     {
-        transform.localScale = new Vector3(
-            transform.localScale.x,
+        progressBar.transform.localScale = new Vector3(
+            progressBar.transform.localScale.x,
             progress * targetHeight,
-            transform.localScale.z);
+            progressBar.transform.localScale.z);
+
+        hull.transform.localScale = new Vector3(
+            hull.transform.localScale.x,
+            targetHeight,
+            hull.transform.localScale.z);
+
+        upperCap.transform.localPosition = new Vector3(
+            upperCap.transform.localPosition.x,
+            targetHeight * 2 + 0.05f,
+            upperCap.transform.localPosition.z);
+
+        GameObject badge = (GameObject)Instantiate(Resources.Load("Badge"));
+        Bounds badgeBounds = Geometry.GetBoundsIndependentFromRotation(badge.transform);
+        badge.transform.parent = transform;
+        badge.transform.localPosition = upperCap.transform.localPosition + new Vector3(0, badgeBounds.size.x + 0.25f, 0);
     }
 
     private void Update()
     {
+        progress = Math.Min(Math.Max(0, progress), 1);
+
         Vector3 targetScale = new Vector3(
-            transform.localScale.x,
+            progressBar.transform.localScale.x,
             progress * targetHeight,
-            transform.localScale.z);
-        transform.localScale = Vector3.SmoothDamp(transform.localScale, targetScale, ref velocity, smoothTime);
+            progressBar.transform.localScale.z);
+        progressBar.transform.localScale = Vector3.SmoothDamp(progressBar.transform.localScale, targetScale, ref velocity, smoothTime);
     }
 }
