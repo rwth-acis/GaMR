f54e809feaf814aa9476e16b08958f6683fa1a21
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2017-07-30 15:26:38 +0200

    improved consistency manager

--- Frontend/GaMR/Assets/Scripts/ConsistencyManager.cs
+++ Frontend/GaMR/Assets/Scripts/ConsistencyManager.cs
@@ -3,23 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsistencyManager : Singleton<ConsistencyManager>
+public class ConsistencyManager : MonoBehaviour
 {
-    protected override void Awake()
+    [Tooltip("Id of the object; There can only be one gameobject with this id in the game. All others are destroyed")]
+    public string id;
+    /// <summary>
+    /// The dictionary which holds the instance for each id
+    /// </summary>
+    private static Dictionary<string, GameObject> instances;
+
+    /// <summary>
+    /// Initializes the dictionary if it has not been initialized
+    /// tests if an instance with the id already exists
+    /// if not: registers this gameobject as the instance and makes it consistent across scenes
+    /// if it exists: destroys it
+    /// </summary>
+    protected void Awake()
     {
-        // there should only be one gameobject in the scene and it is consistent across all scenes
-        // if the instance is already set => destroy this gameobject if it is not the instance
-        if (Instance != null)
+        if (instances == null)
         {
-            if (Instance != this)
+            instances = new Dictionary<string, GameObject>();
+        }
+
+        if (instances.ContainsKey(id) && instances[id] != null)
+        {
+            if (instances[id] != this)
             {
-                Destroy(gameObject);
+                // destroy immediate so that child scripts do not get the chance to run
+                DestroyImmediate(gameObject);
+                Debug.Log("Destroyed duplicate of " + id);
+            }
+        }
+        else
+        {
+            if (instances.ContainsKey(id))
+            {
+                instances[id] = gameObject;
             }
             else
             {
-                DontDestroyOnLoad(gameObject);
+                instances.Add(id, gameObject);
             }
+            DontDestroyOnLoad(gameObject);
         }
-        base.Awake();
     }
 }
--- Frontend/GaMR/Assets/Scripts/Login/AuthorizationManager.cs
+++ Frontend/GaMR/Assets/Scripts/Login/AuthorizationManager.cs
@@ -8,6 +8,14 @@ public class AuthorizationManager : Singleton<AuthorizationManager> {
 
 	public static void Authorize(string name, string password)
     {
+        if (name == "Student")
+        {
+            InformationManager.Instance.playerType = PlayerType.STUDENT;
+        }
+        else
+        {
+            InformationManager.Instance.playerType = PlayerType.AUTHOR;
+        }
         SceneManager.LoadScene("Scene", LoadSceneMode.Single);
     }
 }
