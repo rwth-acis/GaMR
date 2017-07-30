76a47614dd06aa8e9f97ad020dbac3254c262931
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2017-07-30 14:28:34 +0200

    made managers consistent across scenes

---
+++ Frontend/GaMR/Assets/Scripts/ConsistencyManager.cs
@@ -0,0 +1,25 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsistencyManager : Singleton<ConsistencyManager>
+{
+    protected override void Awake()
+    {
+        // there should only be one gameobject in the scene and it is consistent across all scenes
+        // if the instance is already set => destroy this gameobject if it is not the instance
+        if (Instance != null)
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        base.Awake();
+    }
+}
