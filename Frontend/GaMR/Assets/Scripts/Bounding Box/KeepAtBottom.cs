0b13f877bb018b0c5a7e7a661594fcc643744cd6
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2017-11-09 13:41:50 +0100

    adapted bounding box menu to work with 3D rotation

---
+++ Frontend/GaMR/Assets/Scripts/Bounding Box/KeepAtBottom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// script which keeps the transform at the bottom of the parent transform's bounds
+/// the bounds of the parent are defined by a box collider
+/// </summary>
+public class KeepAtBottom : MonoBehaviour
+{
+
+    private BoxCollider coll;
+
+    // Use this for initialization
+    void Start()
+    {
+        coll = transform.parent.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = coll.bounds.center - new Vector3(0, coll.bounds.extents.y, 0);
+    }
+}
