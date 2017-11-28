d8f41b60efc3a41957e1e1074ea3ca26cd57ba3c
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2017-11-28 11:45:55 +0100

    implemented automatic button scaling

--- Frontend/GaMR/Assets/Scripts/MessageBox/ButtonScaler.cs
+++ Frontend/GaMR/Assets/Scripts/MessageBox/ButtonScaler.cs
@@ -6,41 +6,106 @@ using UnityEngine;
 public class ButtonScaler : MonoBehaviour
 {
 #if UNITY_EDITOR
-    Transform frameObject;
-    SpriteRenderer frame;
-    Transform icon;
-    Transform captionObject;
-    Vector3 originalSize;
+    private Transform frameTransform;
+    private SpriteRenderer frame;
+    private Transform icon;
+    private Transform captionTransform;
+    private bool firstUpdate = true;
+    private Vector3 originalSize;
+    private Vector3 ratio = Vector3.one;
+    private Vector2 originalFrameSize;
 
-    void Awake()
+    public float iconSize = 0.05f;
+    public float captionSize = 0.006f;
+    public float borderWidth = 1f;
+    private float frameSize = 0.01423f; // this is an internal value which scales the frame to the correct size
+
+    public bool scaleWithButton = true;
+
+    void Initialize()
     {
-        originalSize = transform.localScale;
-        frameObject = transform.Find("Frame");
-        if (frameObject != null)
+        frameTransform = transform.Find("Frame");
+        if (frameTransform != null)
         {
-            frame = frameObject.GetComponent<SpriteRenderer>();
+            frame = frameTransform.GetComponent<SpriteRenderer>();
         }
 
         icon = transform.Find("Icon");
-        captionObject = transform.Find("Caption");
+        captionTransform = transform.Find("Caption");
+
+        originalSize = transform.localScale;
+        originalFrameSize = frame.size;
     }
 
-    private void UndoScaling(Transform parent, Transform child, float size)
+    private void UndoScaling(Transform child, float scaleFactor)
     {
-        Vector3 newScale = new Vector3(
-            size / parent.localScale.x,
-            size / parent.localScale.y,
-            size / parent.localScale.z
-            );
+        UndoScaling(child, new Vector3(scaleFactor, scaleFactor, scaleFactor));
+    }
 
-        newScale = child.rotation * newScale;
-        child.localScale = newScale;
-        
+    private void UndoScaling(Transform child, Vector3 scaleFactors)
+    {
+        if (transform.localScale.x != 0 && transform.localScale.y != 0 && transform.localScale.z != 0)
+        {
+            Vector3 newScale = new Vector3(
+                scaleFactors.x / transform.localScale.x,
+                scaleFactors.y / transform.localScale.y,
+                scaleFactors.z / transform.localScale.z
+                );
+
+            newScale = child.rotation * newScale;
+            child.localScale = newScale;
+        }
+    }
+
+    private void ScaleProportionally(Transform child)
+    {
+            float scaleFactor = Mathf.Min(ratio.y, ratio.z);
+            child.localScale *= scaleFactor;
+    }
+
+    private void ScaleFrame()
+    {
+        frame.size = new Vector2(
+            originalFrameSize.x * ratio.z,
+            originalFrameSize.y * ratio.y
+            );
+        frame.size /= borderWidth;
+        frame.transform.localScale *= borderWidth;
     }
 
     void Update()
     {
-        UndoScaling(transform, icon, 0.03f);
+        if (firstUpdate)
+        {
+            Initialize();
+            firstUpdate = false;
+        }
+
+        if (borderWidth == 0)
+        {
+            borderWidth = 1;
+        }
+
+        UndoScaling(icon, iconSize);
+        UndoScaling(frameTransform, frameSize);
+        UndoScaling(captionTransform, captionSize);
+
+
+        if (transform.localScale.x != 0 && transform.localScale.y != 0 && transform.localScale.z != 0)
+        {
+            ratio = new Vector3(
+                transform.localScale.x / originalSize.x,
+                transform.localScale.y / originalSize.y,
+                transform.localScale.z / originalSize.z
+                );
+        }
+
+        if (scaleWithButton)
+        {
+            ScaleProportionally(icon);
+            ScaleProportionally(captionTransform);
+        }
+        ScaleFrame();
     }
 #endif
 }
