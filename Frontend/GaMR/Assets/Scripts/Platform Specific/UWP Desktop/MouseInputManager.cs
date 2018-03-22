99806bd26d9a6145fd657f4bfe713fdaf456a863
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-03-22 11:20:33 +0100

    elements now react to mouse clicks

--- Frontend/GaMR/Assets/Scripts/Platform Specific/UWP Desktop/MouseInputManager.cs
+++ Frontend/GaMR/Assets/Scripts/Platform Specific/UWP Desktop/MouseInputManager.cs
@@ -4,23 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MouseInputManager : MonoBehaviour
+public class MouseInputManager : MonoBehaviour, IPointingSource
 {
     GameObject cursorObject;
     Transform lastObjectHit;
     InteractionInputSource inputSource;
+    EventSystem eventSystem;
+
+    public Ray Ray
+    {
+        get;
+        private set;
+    }
+
+    public float? ExtentOverride { get { return 10f; } }
+
+    public LayerMask[] PrioritizedLayerMasksOverride { get { return new LayerMask[] { Physics.DefaultRaycastLayers }; } }
+
+    public bool OwnsInput(BaseEventData eventData)
+    {
+        return true;
+    }
+
+    public void UpdatePointer()
+    {
+        Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
 
     private void Awake()
     {
         cursorObject = GameObject.FindGameObjectWithTag("Cursor");
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        FocusManager.Instance.RegisterPointer(this);
     }
 
     private void FixedUpdate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 10f))
+        Debug.DrawRay(Ray.origin, Ray.direction);
+
+        if (Physics.Raycast(Ray, out hit, 100f))
         {
             cursorObject.transform.position = hit.point - 0.02f * (hit.point - Camera.main.transform.position);
             Quaternion.LookRotation(hit.normal);
@@ -35,6 +60,7 @@ public class MouseInputManager : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 Debug.Log("Down: " + objectHit);
+                InputManager.Instance.RaiseInputClicked(null, 0, InteractionSourcePressInfo.Select, 1, null);
             }
 
             if (objectHit != lastObjectHit)
@@ -53,7 +79,7 @@ public class MouseInputManager : MonoBehaviour
         }
         else
         {
-            cursorObject.transform.position = ray.origin + 2 * ray.direction;
+            cursorObject.transform.position = Ray.origin + 2 * Ray.direction;
         }
     }
 }
