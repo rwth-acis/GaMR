dff300e3d901e93ba1d200c844c90f54287f04b4
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-03-22 12:00:37 +0100

    added mouse as IInputSource

--- Frontend/GaMR/Assets/Scripts/Platform Specific/UWP Desktop/MouseInputManager.cs
+++ Frontend/GaMR/Assets/Scripts/Platform Specific/UWP Desktop/MouseInputManager.cs
@@ -4,11 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MouseInputManager : MonoBehaviour, IPointingSource
+public class MouseInputManager : MonoBehaviour, IPointingSource, IInputSource
 {
     GameObject cursorObject;
     Transform lastObjectHit;
-    InteractionInputSource inputSource;
     EventSystem eventSystem;
 
     public Ray Ray
@@ -21,9 +20,79 @@ public class MouseInputManager : MonoBehaviour, IPointingSource
 
     public LayerMask[] PrioritizedLayerMasksOverride { get { return new LayerMask[] { Physics.DefaultRaycastLayers }; } }
 
+    public SupportedInputInfo GetSupportedInputInfo(uint sourceId)
+    {
+        return SupportedInputInfo.Select;
+    }
+
     public bool OwnsInput(BaseEventData eventData)
     {
         return true;
+    }
+
+    public bool SupportsInputInfo(uint sourceId, SupportedInputInfo inputInfo)
+    {
+        return true;
+    }
+
+    public bool TryGetGrasp(uint sourceId, out bool isPressed)
+    {
+        isPressed = false;
+        return false;
+    }
+
+    public bool TryGetGripPosition(uint sourceId, out Vector3 position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetGripRotation(uint sourceId, out Quaternion rotation)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetMenu(uint sourceId, out bool isPressed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetPointerPosition(uint sourceId, out Vector3 position)
+    {
+        position = Input.mousePosition;
+        return true;
+    }
+
+    public bool TryGetPointerRotation(uint sourceId, out Quaternion rotation)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetPointingRay(uint sourceId, out Ray pointingRay)
+    {
+        pointingRay = Ray;
+        return true;
+    }
+
+    public bool TryGetSelect(uint sourceId, out bool isPressed, out double pressedValue)
+    {
+        isPressed = Input.GetMouseButtonDown(0);
+        pressedValue = 1f;
+        return true;
+    }
+
+    public bool TryGetSourceKind(uint sourceId, out InteractionSourceInfo sourceKind)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetThumbstick(uint sourceId, out bool isPressed, out Vector2 position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetTouchpad(uint sourceId, out bool isPressed, out bool isTouched, out Vector2 position)
+    {
+        throw new System.NotImplementedException();
     }
 
     public void UpdatePointer()
@@ -40,7 +109,6 @@ public class MouseInputManager : MonoBehaviour, IPointingSource
 
     private void FixedUpdate()
     {
-        Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
         Debug.DrawRay(Ray.origin, Ray.direction);
@@ -52,15 +120,17 @@ public class MouseInputManager : MonoBehaviour, IPointingSource
 
             Transform objectHit = hit.transform;
 
-            if (Input.GetMouseButton(0))
-            {
-                Debug.Log("Clicked: " + objectHit);
-            }
-
             if (Input.GetMouseButtonDown(0))
             {
                 Debug.Log("Down: " + objectHit);
-                InputManager.Instance.RaiseInputClicked(null, 0, InteractionSourcePressInfo.Select, 1, null);
+                InputManager.Instance.RaiseSourceDown(this, 0, InteractionSourcePressInfo.Select, null);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Debug.Log("Clicked: " + objectHit);
+                InputManager.Instance.RaiseSourceUp(this, 0, InteractionSourcePressInfo.Select, null);
+                InputManager.Instance.RaiseInputClicked(this, 0, InteractionSourcePressInfo.Select, 1, null);
             }
 
             if (objectHit != lastObjectHit)
