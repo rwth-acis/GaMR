8bed0d4739c5249204f0c94527939380bf7a8313
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-04-19 11:50:17 +0200

    added custom desktop solution for tap to place

--- Frontend/GaMR/Assets/Scripts/Platform Specific/KeepInFrontOfCamera.cs
+++ Frontend/GaMR/Assets/Scripts/Platform Specific/KeepInFrontOfCamera.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeepInFrontOfCamera : MonoBehaviour {
+public class KeepInFrontOfCamera : MonoBehaviour, IInputClickHandler
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float distanceInFrontOfCamera = 2f;
+    public bool isBeingPlaced = false;
+
+    public virtual void OnInputClicked(InputClickedEventData eventData)
+    {
+        isBeingPlaced = !isBeingPlaced;
+    }
+
+    private void Update()
+    {
+        if (isBeingPlaced)
+        {
+            transform.position =
+                Camera.main.transform.position + distanceInFrontOfCamera * Camera.main.transform.forward;
+        }
+    }
 }
