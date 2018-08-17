12108438e70a67f90bf792e898ed557cfa190c6d
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-08-17 11:56:52 +0200

    added a version counter

---
+++ Frontend/GaMR/Assets/Scripts/VersionManager.cs
@@ -0,0 +1,22 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionManager : Singleton<VersionManager>
+{
+    [SerializeField]
+    private uint majorRelease = 1;
+    [SerializeField]
+    private uint minorRelease = 0;
+    [SerializeField]
+    private uint patchLevel = 0;
+
+    public uint MajorRelease { get { return majorRelease; } }
+
+    public uint MinorRelease { get { return minorRelease; } }
+
+    public uint PatchLevel { get { return patchLevel; } }
+
+    public string VersionString { get { return MajorRelease + "." + MinorRelease + "." + PatchLevel; } }
+}
