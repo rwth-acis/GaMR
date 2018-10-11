c786678ba5620879331b3c67fffa4b6d8b17865c
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-10-11 11:37:31 +0200

    first prototype for UI animations

--- Frontend/GaMR/Assets/Scripts/Platform Specific/UIManager.cs
+++ Frontend/GaMR/Assets/Scripts/Platform Specific/UIManager.cs
@@ -6,8 +6,17 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public CanvasScaler canvasScaler;
-    public Transform mainPage;
+    public Transform loginPage;
     public Transform settingsPage;
+
+    private Animator loginPageAnimator;
+    private Animator settingsPageAnimator;
+
+    private void Awake()
+    {
+        loginPageAnimator = loginPage.GetComponent<Animator>();
+        settingsPageAnimator = settingsPage.GetComponent<Animator>();
+    }
 
     public void Login()
     {
@@ -16,7 +25,12 @@ public class UIManager : MonoBehaviour
 
     public void ShowSettings()
     {
-        
+        loginPageAnimator.SetBool("LoginMenuVisible", !loginPageAnimator.GetBool("LoginMenuVisible"));
+        settingsPageAnimator.SetBool("SettingsVisible", !settingsPageAnimator.GetBool("SettingsVisible"));
+    }
+
+    private void Update()
+    {
     }
 
 }
