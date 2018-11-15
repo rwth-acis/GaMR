e1e15fea09e00a50cc7493992da9240af4d674de
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-11-15 13:07:45 +0100

    implemented and added menu movement script for floating menus

--- Frontend/GaMR/Assets/Scripts/Menu/MoveMenu.cs
+++ Frontend/GaMR/Assets/Scripts/Menu/MoveMenu.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class MoveMenu : MonoBehaviour, IFocusable, IManipulationHandler
 {
-    public Vector3 rotationOffset;
+    public Vector3 realForward;
+    private float speedFactor;
 
     private Vector3 startingPoint;
     private Transform globalParent;
 
     public void OnFocusEnter()
     {
-
     }
 
     public void OnFocusExit()
@@ -21,16 +21,20 @@ public class MoveMenu : MonoBehaviour, IFocusable, IManipulationHandler
 
     public void OnManipulationCanceled(ManipulationEventData eventData)
     {
+        InputManager.Instance.OverrideFocusedObject = null;
     }
 
     public void OnManipulationCompleted(ManipulationEventData eventData)
     {
+        InputManager.Instance.OverrideFocusedObject = null;
     }
 
     public void OnManipulationStarted(ManipulationEventData eventData)
     {
         globalParent = GetGlobalParent();
-        startingPoint = transform.position;
+        startingPoint = globalParent.position;
+
+        InputManager.Instance.OverrideFocusedObject = gameObject;
     }
 
     public void OnManipulationUpdated(ManipulationEventData eventData)
@@ -39,10 +43,14 @@ public class MoveMenu : MonoBehaviour, IFocusable, IManipulationHandler
         {
             globalParent = transform;
         }
-        globalParent.position = startingPoint + eventData.CumulativeDelta;
 
-        globalParent.transform.LookAt(Camera.main.transform);
-        globalParent.transform.eulerAngles += rotationOffset;
+        Vector3 vectorToCam = Camera.main.transform.position - globalParent.transform.position;
+
+        speedFactor = vectorToCam.magnitude;
+        globalParent.position = startingPoint + speedFactor * eventData.CumulativeDelta;
+
+        globalParent.transform.rotation = Quaternion.LookRotation(vectorToCam, Vector3.up);
+        globalParent.transform.rotation = Quaternion.LookRotation(globalParent.transform.TransformDirection(realForward));
     }
 
     private Transform GetGlobalParent()
