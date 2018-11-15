d18fcd23c12dd38125cad40d5d94e900f95303e0
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-11-15 10:51:21 +0100

    implemented a different method to move menus

---
+++ Frontend/GaMR/Assets/Scripts/Menu/MoveMenu.cs
@@ -0,0 +1,58 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMenu : MonoBehaviour, IFocusable, IManipulationHandler
+{
+    public Vector3 rotationOffset;
+
+    private Vector3 startingPoint;
+    private Transform globalParent;
+
+    public void OnFocusEnter()
+    {
+
+    }
+
+    public void OnFocusExit()
+    {
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        globalParent = GetGlobalParent();
+        startingPoint = transform.position;
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        if (globalParent == null)
+        {
+            globalParent = transform;
+        }
+        globalParent.position = startingPoint + eventData.CumulativeDelta;
+
+        globalParent.transform.LookAt(Camera.main.transform);
+        globalParent.transform.eulerAngles += rotationOffset;
+    }
+
+    private Transform GetGlobalParent()
+    {
+        Transform current = transform;
+        while (current.parent != null)
+        {
+            current = current.parent;
+        }
+
+        return current;
+    }
+}
