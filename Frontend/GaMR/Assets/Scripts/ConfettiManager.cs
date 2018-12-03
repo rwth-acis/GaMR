af07dc5da2cd2d2a842c7c4237b3ffc388422ed8
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2018-12-03 12:18:42 +0100

    implemented confetti script and particle system for quiz winning animation

---
+++ Frontend/GaMR/Assets/Scripts/ConfettiManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiManager : LocalSingleton<ConfettiManager>
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+        gameObject.SetActive(false);
+    }
+
+    public void ShootConfetti(Vector3 position)
+    {
+        gameObject.SetActive(false);
+        gameObject.transform.position = position;
+        gameObject.SetActive(true);
+    }
+}
---
+++ Frontend/GaMR/Assets/Scripts/Utilities/LocalSingleton.cs
@@ -0,0 +1,36 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalSingleton<T> : MonoBehaviour where T : LocalSingleton<T>
+{
+    public static T Instance
+    {
+        get;private set;
+    }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            if (Instance != this)
+            {
+                Destroy(this);
+                Debug.LogWarning("Destroyed second singleton instance of " + gameObject.name);
+            }
+        }
+        else
+        {
+            Instance = (T)this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
